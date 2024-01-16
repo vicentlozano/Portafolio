32168fe646ff20eeaf5c2bac723080d952827004
vicentlozano <lozanodroid@gmail.com>
2024-01-16 15:32:39 +0100

    fgfgsfsfg

--- .netlify/functions-internal/ssr/chunks/Contact_9HAzAlV0.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/Contact_Q-M6tbLu.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_gWjZqPcd.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/index_Zzk_W0_L.mjs');
-
-export { page };
